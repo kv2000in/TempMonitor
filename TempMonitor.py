01bf86cfe9819780acd2170df2ee743e9fed99df
Kumar Vivek <kv2000in@mac.local>
2018-12-15 15:48:33 -0500

    URL and Sensor variables added

--- TempMonitor.py
+++ TempMonitor.py
@@ -2,13 +2,15 @@
 #ipwebcam sensor data to CSV file
 import re
 #mystring = '{"ambient_temp":{"unit":"℃","data":[[1544568579352,[24.980392]],[1544568580314,[24.980392]],[1544568581275,[24.980392]],[1544568582236,[24.980392]],[1544568583194,[24.980392]],[1544568584154,[24.980392]]]}}' # each line
-
 import time
 import urllib2
 #contents = urllib2.urlopen("http://192.168.1.126:8080/sensors.json?sense=ambient_temp").read()
+myurl="http://192.168.1.126:8080/"
+mysensor="ambient_temp"
 while 1:
 	myfile = open('tempdata.csv','a')
-	mystring = urllib2.urlopen("http://192.168.1.126:8080/sensors.json?sense=ambient_temp").read()
+	#mystring = urllib2.urlopen("http://192.168.1.126:8080/sensors.json?sense=ambient_temp").read()
+	mystring = urllib2.urlopen(myurl+"sensors.json?sense="+mysensor).read()
 	myregex  = r'\S*:\S*:\S*:(\S*)]}}'
 	mytxt = re.search(myregex,mystring)
 	myregex1 = r'.{1,2}(\d*),\[(\d*\.\d*)\]\]'
