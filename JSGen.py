a9c645b20bce6e8462486d26fe5bf963329c0833
Kumar Vivek <kv2000in@mac.local>
2018-12-15 17:04:07 -0500

    added files for plotly

---
+++ JSGen.py
@@ -0,0 +1,24 @@
+#Copyright kv2000in@gmail.com
+#ipwebcam sensor data to CSV file
+#Usage : python TempMonitor.py http://192.168.1.126:8080/ ambient_temp
+# Will save ambient_temp data in ambient_temp.csv file
+import sys
+import re
+import time
+import urllib2
+myurl = sys.argv[1]
+mysensor = sys.argv[2]
+while 1:
+	myfile = open(mysensor+'.js','a')
+	mystring = urllib2.urlopen(myurl+"sensors.json?sense="+mysensor).read()
+	myregex  = r'\S*:\S*:\S*:(\S*)]}}'#\S any non whitespace
+	mytxt = re.search(myregex,mystring)
+	myregex1 = r'.{1,2}(\d*),\[(\d*\.\d*)\]\]'
+	mytxt1=re.findall(myregex1,mytxt.group(1))
+	i = 0
+	while i <len(mytxt1): 
+		myfile.write(traceX=mytxt1[i][0]+","+mytxt1[i][1])
+		i +=1
+		myfile.write('\n')
+	myfile.close()
+	time.sleep(5)
